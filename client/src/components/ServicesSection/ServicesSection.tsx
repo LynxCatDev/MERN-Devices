2f6d523d92037e7fdf8a6ee18a5e8d94e6212253
Theodore <uzuntudor@yahoo.com>
2025-09-20 14:09:27 +0300

    accesability for images and css

--- client/src/components/ServicesSection/ServicesSection.tsx
+++ client/src/components/ServicesSection/ServicesSection.tsx
@@ -12,16 +12,17 @@ export const ServicesSection = () => {
     <div className="service-section">
       <div className="smart-protection service">
         <Link href={`/protection`} aria-label="Smart Protection details">
-          <Image
-            src="/images/offer-day.webp"
-            alt="offer-day"
-            width={0}
-            height={0}
-            sizes="48px"
-            style={{ width: 48, height: 'auto' }}
-            loading="lazy"
-            priority={false}
-          />
+          <div className="service--image">
+            <Image
+              src="/images/offer-day.webp"
+              alt="offer-day"
+              fill
+              sizes="(max-width:576px) 60px, (max-width:768px) 70px, (max-width:992px) 80px, 115px"
+              style={{ objectFit: 'contain' }}
+              loading="lazy"
+              priority={false}
+            />
+          </div>
           <div className="service--card">
             <h3>{t('offer_day_title')}</h3>
             <span>{t('offer_day')}</span>
@@ -33,16 +34,18 @@ export const ServicesSection = () => {
       </div>
       <div className="trade-in service">
         <Link href={`/tradein`} aria-label="Trade-in details">
-          <Image
-            src="/images/trade-in.webp"
-            alt="trade-in"
-            width={0}
-            height={0}
-            sizes="48px"
-            style={{ width: 48, height: 'auto' }}
-            loading="lazy"
-            priority={false}
-          />
+          <div className="service--image">
+            <Image
+              src="/images/trade-in.webp"
+              alt="trade-in"
+              width={0}
+              height={0}
+              sizes="48px"
+              style={{ width: 48, height: 'auto' }}
+              loading="lazy"
+              priority={false}
+            />
+          </div>
           <div className="service--card">
             <h3>Trade In</h3>
             <span>{t('trade_in')}</span>
